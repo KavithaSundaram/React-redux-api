b040dc6e553de54ab856d360639912fb5185ed49
KavithaSundaram <kavithamsc1707@gmail.com>
2023-12-01 01:14:55 -0800

    React file upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
 
 function App() {
+const[fake,setFake]=useState([])
+console.log(fake)
+useEffect(()=>{
+  fakestore()
+},[])
+
+  const fakestore=async()=>{
+    const response=await fetch("https://fakestoreapi.com/products")
+    //console.log(response)
+    const jsonData=await response.json()
+    //console.log(jsonData)
+    setFake(jsonData)
+  }
+  //fakestore()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h2>Fake Api store</h2>
+    <div className="container">
+      <div className="row">
+      {fake.map((values)=>{
+        return(
+          <>
+            <div className="col-sm-12 col-lg-4 col-md-6">
+              <div className="box">
+                <div className="content">
+                  <h5>{values.title}</h5>
+                  <p>{values.description}</p>
+                  <img src={values.image} alt="" />
+                </div>
+              </div>
+            </div>
+          </>
+        )
+      })}
+      </div>
     </div>
+    </>  
   );
 }
 
